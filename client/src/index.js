2a24756774a56728ba058cb70ee11597b43b4d91
leuneri <ericleung147258369@hotmail.com>
2023-05-25 14:49:22 -0800

    set up for initial state and created redux store that wraps around application, access local storage before rendering

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,46 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import authReducer from "./state";
+import { configureStore } from "@reduxjs/toolkit";
+import { Provider } from 'react-redux';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDTATE,
+  PAUSE,
+  PURGE,
+  REGISTER
+} from "redux-persist"; // save state into local storage -> close tab, info still there unless clear cache -> login, data, etc.
+import storage from "redux-persist/lib/storage";
+import { PersistGate } from "redux-persist/integration/react";
+
+const persistConfig = { key: "root", storage, version: 1 }; // root is which persisted state will be stored,  storage from package represents default web storage used for persisting,  version to be persisted
+const persistedReducer = persistReducer(persistConfig, authReducer); // create new reducer (function for how app state state should change) with previously defined config object and original state shown as authReducer from ./state
+const store = configureStore({
+  // resulting store object represents the configured Redux store with the persisted state and customized middleware
+  // redux store is object that holds app state, allows state access, dispatch actions to store -> executes reducers that change states, updates state, notifies subscribers after updates
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [  FLUSH, REHYDTATE, PAUSE, PURGE, REGISTER ]
+      }
+    })
+// code sets up Redux state persistence using redux-persist, creates a new reducer that handles persistence, and configures a Redux store with the persisted reducer and customized middleware
+})
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}> 
+    {/* Provider: Allows App to have access to Redux store created */}
+      <PersistGate loading={null} persistor={persistStore(store)}>
+        {/* PersistGate: Ensure persisted state is loaded before rendering application -> restore state from local storage before rendering UI components */}
+        <App />
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
--- client/src/state/index.js
+++ client/src/state/index.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+// createSlice takes initial state object and has reducers that change the state
 
 const initialState = {
     // state stored in global state -> data accessible anywhere
@@ -9,13 +10,41 @@ const initialState = {
 };
 
 export const authSlice = createSlice({
-    // functions 
     name: "auth",
     initialState,
     reducers: {
         setMode: (state) => {
             state.mode = state.mode === "light" ? "dark" : "light";
+        },
+        setLogin: (state, action) => {
+            state.user = action.payload.user;
+            state.token = action.payload.token;
+            // action.payload data typically comes from user input, API responses, computed values or local data stored in application
+        },
+        setLogout: (state) => {
+            state.user = null;
+            state.token = null;
+        },
+        setFriends: (state, action) => {
+            if (state.user) {
+                state.user.friends = action.payload.friends;
+            } else {
+                console.error("user friends non-existent :(")
+            }
+        },
+        setPosts: (state, action) => {
+            state.posts = action.payload.posts;
+        },
+        setPost: (state, action) => {
+            const updatedPosts = state.posts.map((post) => {
+                if (post._id === action.payload.post_id) return action.payload.post;
+                return post;
+            })
+            state.posts = updatedPosts;
         }
     }
 
 })
+
+export const { setMode, seetLogin, setLogout, setFriends, setPosts, setPost } = authSlice.actions;
+export default authSlice.reducer;
